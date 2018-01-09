700c9e6b3f4e1bf5d5d0c872f3c84071c083f585
Steve Geluso <stevegeluso@gmail.com>
2018-01-09 12:49:40 -0800

    lifting state, separating search form and result list

--- 27-forms-and-props/demos/forms/src/components/result-list.js
+++ 27-forms-and-props/demos/forms/src/components/result-list.js
@@ -1,28 +1,19 @@
 import React from 'react'
 
 class ResultList extends React.Component {
-  constructor(props) {
-    super(props)
-    let newResults = [
-        {language: 'python', rating: 8},
-        {language: 'java', rating: 6},
-        {language: 'javascript', rating: 9},
-        {language: 'php', rating: 2},
-    ]
-    this.state = {
-      //results: []
-      results: newResults
-    }
-  }
-
   languageList() {
-    if (this.state.results.length === 0) {
+    if (!this.props.hasSearched) {
+      return <div></div>
+    } else if (this.props.loading) {
+      return <p>Loading...</p>
+    } else if (this.props.results.length === 0) {
       return <p>No results.</p>
     } else {
-      return this.state.results.map((result, index)=> {
-        return <div key={index}>
-          <p>Rating: {result.rating}</p>
-          <p>Language: {result.language}</p>
+      return this.props.results.map((result, index)=> {
+        return <div className="language-rating" key={index}>
+          Rating: {result.rating}
+          &nbsp;
+          Language: {result.language}
         </div>
       });
     }
