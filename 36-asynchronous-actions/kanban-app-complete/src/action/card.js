d54c468c58f42ab759d82e244e62be7ce18c415d
Vinicio <vinicio@codefellows.com>
2018-01-23 13:40:38 -0800

    full stack login/signup

---
+++ 36-asynchronous-actions/kanban-app-complete/src/action/card.js
@@ -0,0 +1,18 @@
+export const createAction = ({content,sectionID}) => ({
+  type: 'CARD_CREATE',
+  payload : {
+    content,
+    sectionID,
+    id: btoa(Math.random()),
+  }
+});
+
+export const updateAction = (card) => ({
+  type : 'CARD_UPDATE',
+  payload : card,
+});
+
+export const removeAction = (card) => ({
+  type : 'CARD_REMOVE',
+  payload : card,
+});
